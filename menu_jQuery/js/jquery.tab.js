15048edd37ce50d4252a33e8b27c34c7f608653f
murase <noxon.ff@gmail.com>
2019-12-02 11:50:29 +0900

    プラグイン化させたその2

--- menu_jQuery/js/jquery.tab.js
+++ menu_jQuery/js/jquery.tab.js
@@ -1,83 +1,66 @@
-;
+(function () {
+    $.fn.tab = function (options) {
 
-(function ($) {
-    $.fn.tab = function () {
+        // メニューの配列を作る
+        const menuAry = $(this).find('li');
 
-        function tab(options) {
-            // メニューの配列を作る
-            const menuAry = $(options.menu).find('li');
+        // コンテンツの箱の配列を作る
+        const contentSectionAry = $(options.section).find('section');
 
-            // コンテンツの箱の配列を作る
-            const contentSectionAry = $(options.section).find('section');
+        //次へ、戻るを変数に入れる
+        const prevElement = $(options.tabControle).find('.prev');
 
-            //次へ、戻るを変数に入れる
-            const prevElement = $(options.tabControle).find('.prev');
+        const nextElement = $(options.tabControle).find('.next');
 
-            const nextElement = $(options.tabControle).find('.next');
+        //メニューをクリックしたら
+        $(this).each(function () {
+            $(this).on('click', onClickMenu);
+        });
 
-            menuAry.on('click', onClickMenu)
+        // クリックしたところが何番目か返して
+        function onClickMenu() {
+            const index = menuAry.index(this);
+            move(index);
+        }
 
-            // クリックしたところが何番目か返して
-            function onClickMenu() {
-                const index = menuAry.index(this);
-                move(index);
-            }
+        let currentNum = 0;
 
-            let currentNum = 0;
+        move(options.defaultNum);
 
-            move(options.defaultNum);
+        prevElement.on('click', onClickPrev);
 
-            prevElement.on('click', onClickPrev);
+        function onClickPrev() {
+            move(currentNum - 1);
+        }
 
-            function onClickPrev() {
-                move(currentNum - 1);
-            }
+        nextElement.on('click', onClickNext);
 
-            nextElement.on('click', onClickNext);
+        function onClickNext() {
+            move(currentNum + 1);
+        }
 
-            function onClickNext() {
-                move(currentNum + 1);
-            }
+        function move(num) {
 
-            function move(num) {
-
-                if (options.isLoop) {
-                    if (num >= menuAry.length) {
-                        currentNum = 0;
-                    } else if (num < 0) {
-                        currentNum = menuAry.length - 1;
-                    } else {
-                        currentNum = num;
-                    }
+            if (options.isLoop) {
+                if (num >= menuAry.length) {
+                    currentNum = 0;
+                } else if (num < 0) {
+                    currentNum = menuAry.length - 1;
                 } else {
-                    currentNum = Math.min((menuAry.length - 1), num);
-                    currentNum = Math.max(0, currentNum);
+                    currentNum = num;
                 }
-
-                // 全部のclassを外す
-                menuAry.removeClass('on');
-                contentSectionAry.removeClass('on');
-
-                // 同じ番号のcontentなどにclassをつける
-                menuAry.eq(currentNum).addClass('on');
-                contentSectionAry.eq(currentNum).addClass('on');
+            } else {
+                currentNum = Math.min((menuAry.length - 1), num);
+                currentNum = Math.max(0, currentNum);
             }
+
+            // 全部のclassを外す
+            menuAry.removeClass('on');
+            contentSectionAry.removeClass('on');
+
+            // 同じ番号のcontentなどにclassをつける
+            menuAry.eq(currentNum).addClass('on');
+            contentSectionAry.eq(currentNum).addClass('on');
         }
     }
 })(jQuery);
-
-tab({
-    defaultNum: 0,
-    menu: document.querySelector('.tabMenu'),
-    section: document.querySelector('.tabContents'),
-    isLoop: true,
-    tabControle: document.querySelector('.tabControle')
-})
-
-tab({
-    defaultNum: 0,
-    menu: document.querySelector('.tabMenu2'),
-    section: document.querySelector('.tabContents2'),
-    isLoop: false,
-    tabControle: document.querySelector('.tabControle2')
-})
